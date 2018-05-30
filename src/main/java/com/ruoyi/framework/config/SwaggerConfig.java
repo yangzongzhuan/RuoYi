41c857647984a2f2d6c502de0adab42df1f2cc04
yangzhengze <1403014932@qq.com>
2018-05-30 11:18:16 +0800

    优化Swagger2,增加测试类

--- src/main/java/com/ruoyi/framework/config/SwaggerConfig.java
+++ src/main/java/com/ruoyi/framework/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.ruoyi.framework.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -17,24 +18,35 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    /** 系统基础配置*/
+    @Autowired
+    private RuoYiConfig ruoYiConfig;
+
+    /**
+     * 创建API
+     * @return
+     */
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                //详细定制
                 .apiInfo(apiInfo())
                 .select()
                 //.apis(RequestHandlerSelectors.basePackage("com.ruoyi.project.*.*.controller"))
+                //扫描所有
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
 
     private ApiInfo apiInfo() {
+        // 用ApiInfoBuilder进行定制
         return new ApiInfoBuilder()
                 .title("系统接口列表")
                 .description("API接口测试平台\",\n" +
                         "                \"提供后台所有Restful接口\",")
                 .termsOfServiceUrl("http://localhost/swagger-ui.html")
-                .contact(new Contact("RuoYi", "https://gitee.com/y_project/RuoYi", "1403014932@qq.com"))
+                .contact(new Contact(ruoYiConfig.getName(), "https://gitee.com/y_project/RuoYi", "1403014932@qq.com"))
                 .version("1.1.0")
                 .build();
     }
