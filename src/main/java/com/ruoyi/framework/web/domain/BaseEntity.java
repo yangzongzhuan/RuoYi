0414c25f9a719936f4dfade6344a313888dd1909
RuoYi <yzz_ivy@163.com>
2018-07-13 22:54:49 +0800

    支持时间查询（操作日志，登录日志，调度日志）

--- src/main/java/com/ruoyi/framework/web/domain/BaseEntity.java
+++ src/main/java/com/ruoyi/framework/web/domain/BaseEntity.java
@@ -2,6 +2,8 @@ package com.ruoyi.framework.web.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Map;
+
 import com.ruoyi.common.utils.DateUtils;
 
 /**
@@ -12,18 +14,27 @@ import com.ruoyi.common.utils.DateUtils;
 public class BaseEntity implements Serializable
 {
     private static final long serialVersionUID = 1L;
+
     /** 搜索值 */
     private String searchValue;
+
     /** 创建者 */
     private String createBy;
+
     /** 创建时间 */
     private Date createTime;
+
     /** 更新者 */
     private String updateBy;
+
     /** 更新时间 */
     private Date updateTime;
+
     /** 备注 */
     private String remark;
+
+    /** 请求参数 */
+    private Map<String, Object> reqParams;
 
     public String getSearchValue()
     {
@@ -93,6 +104,16 @@ public class BaseEntity implements Serializable
     public void setRemark(String remark)
     {
         this.remark = remark;
+    }
+
+    public Map<String, Object> getReqParams()
+    {
+        return reqParams;
+    }
+
+    public void setReqParams(Map<String, Object> reqParams)
+    {
+        this.reqParams = reqParams;
     }
 
 }
