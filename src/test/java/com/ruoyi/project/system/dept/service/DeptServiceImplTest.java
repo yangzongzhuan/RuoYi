6fb64942238f8c743f0a0ea277ac7577d62feeb5
Leonhardt92 <zero_fire@126.com>
2018-07-22 13:43:12 +0800

    提交测试用例

---
+++ src/test/java/com/ruoyi/project/system/dept/service/DeptServiceImplTest.java
@@ -0,0 +1,106 @@
+package com.ruoyi.project.system.dept.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * DeptServiceImpl Tester.
+ *
+ * @author Leonhardt
+ * @version 1.0
+ * @since 07/22/2018
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class DeptServiceImplTest {
+    @Autowired
+    private IDeptService deptService;
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: selectDeptList(Dept dept)
+     */
+    @Test
+    public void testSelectDeptList() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: selectDeptAll()
+     */
+    @Test
+    public void testSelectDeptAll() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: selectDeptTree()
+     */
+    @Test
+    public void testSelectDeptTree() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: selectDeptCount(Long parentId)
+     */
+    @Test
+    public void testSelectDeptCount() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: checkDeptExistUser(Long deptId)
+     */
+    @Test
+    public void testCheckDeptExistUser() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: deleteDeptById(Long deptId)
+     */
+    @Test
+    public void testDeleteDeptById() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: saveDept(Dept dept)
+     */
+    @Test
+    public void testSaveDept() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: selectDeptById(Long deptId)
+     */
+    @Test
+    public void testSelectDeptById() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: checkDeptNameUnique(Dept dept)
+     */
+    @Test
+    public void testCheckDeptNameUnique() throws Exception {
+        //TODO: Test goes here...
+    }
+
+
+} 
