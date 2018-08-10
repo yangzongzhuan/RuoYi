bb7799c1aa9ee3390341e1388bd6d30005c5e381
RuoYi <yzz_ivy@163.com>
2018-08-10 14:37:33 +0800

    新增地址开关

--- src/main/java/com/ruoyi/framework/config/RuoYiConfig.java
+++ src/main/java/com/ruoyi/framework/config/RuoYiConfig.java
@@ -1,6 +1,5 @@
 package com.ruoyi.framework.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
