e93e0c4cb78f0ee9d3c75a50121994d563fa4fdc
RuoYi <yzz_ivy@163.com>
2018-08-15 14:00:23 +0800

    format

--- src/main/java/com/ruoyi/framework/manager/AsyncManager.java
+++ src/main/java/com/ruoyi/framework/manager/AsyncManager.java
@@ -9,18 +9,35 @@ import java.util.concurrent.TimeUnit;
  * 
  * @author liuhulu
  */
-public class AsyncManager {
-	// 操作延迟
-	private final int OPERATE_DELAY_TIME = 10;
-	// 异步操作此案城池
-	private ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(5);
-	// 单例
-	private static AsyncManager me = new AsyncManager();
-	public static AsyncManager me() {
-		return me;
-	}
-	// 执行任务
-	public void execute(TimerTask task) {
-		executor.schedule(task, OPERATE_DELAY_TIME, TimeUnit.MILLISECONDS);
-	}
+public class AsyncManager
+{
+    /**
+     * 操作延迟10毫秒
+     */
+    private final int OPERATE_DELAY_TIME = 10;
+
+    /**
+     * 异步操作任务调度线程池
+     */
+    private ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(5);
+
+    /**
+     * 单例模式
+     */
+    private static AsyncManager me = new AsyncManager();
+
+    public static AsyncManager me()
+    {
+        return me;
+    }
+
+    /**
+     * 执行任务
+     * 
+     * @param 任务task
+     */
+    public void execute(TimerTask task)
+    {
+        executor.schedule(task, OPERATE_DELAY_TIME, TimeUnit.MILLISECONDS);
+    }
 }
