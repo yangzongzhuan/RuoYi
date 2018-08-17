cac513050420b1047419cc12570eecf20eef1456
RuoYi <yzz_ivy@163.com>
2018-08-17 14:55:10 +0800

    优化表格样式~

--- src/main/java/com/ruoyi/framework/config/ScheduleConfig.java
+++ src/main/java/com/ruoyi/framework/config/ScheduleConfig.java
@@ -38,6 +38,8 @@ public class ScheduleConfig
         prop.put("org.quartz.jobStore.maxMisfiresToHandleAtATime", "1");
         prop.put("org.quartz.jobStore.txIsolationLevelSerializable", "true");
 
+        // sqlserver 启用
+        //prop.put("org.quartz.jobStore.selectWithLockSQL", "SELECT * FROM {0}LOCKS UPDLOCK WHERE LOCK_NAME = ?");
         prop.put("org.quartz.jobStore.misfireThreshold", "12000");
         prop.put("org.quartz.jobStore.tablePrefix", "QRTZ_");
         factory.setQuartzProperties(prop);
