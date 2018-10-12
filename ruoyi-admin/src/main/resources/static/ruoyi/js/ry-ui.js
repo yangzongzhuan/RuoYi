9660e30ca5de5c29417a9385e3dc8acd0264bdce
andy <abcltb@163.com>
2018-10-12 13:21:08 +0800

    增加表格是否行间隔色的option配置项

--- ruoyi-admin/src/main/resources/static/ruoyi/js/ry-ui.js
+++ ruoyi-admin/src/main/resources/static/ruoyi/js/ry-ui.js
@@ -21,6 +21,7 @@
                     contentType: "application/x-www-form-urlencoded",   // 编码类型
                     method: 'post',                                     // 请求方式（*）
                     cache: false,                                       // 是否使用缓存
+                    striped: $.common.visible(options.striped),          // 是否显示行间隔色
                     sortable: true,                                     // 是否启用排序
                     sortStable: true,                                   // 设置为 true 将获得稳定的排序
                     sortName: _sortName,                                // 排序列名称
