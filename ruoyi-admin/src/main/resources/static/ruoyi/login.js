583b06ca9a323e0a09ac89447aca7be2b4e358a4
RuoYi <yzz_ivy@163.com>
2018-12-12 13:26:01 +0800

    登录界面去除iCheck插件改为自定义css（提升响应速度及部分背景图不适应）

--- ruoyi-admin/src/main/resources/static/ruoyi/login.js
+++ ruoyi-admin/src/main/resources/static/ruoyi/login.js
@@ -1,7 +1,6 @@
 
 $(function() {
     validateRule();
-    $(".i-checks").iCheck({checkboxClass:"icheckbox_square-green-login"});
 	$('.imgcode').click(function() {
 		var url = ctx + "captcha/captchaImage?type=" + captchaType + "&s=" + Math.random();
 		$(".imgcode").attr("src", url);
