35df1dbcdf71e0ff49b490fb375848e2d5cf0935
RuoYi <yzz_ivy@163.com>
2018-12-14 12:30:33 +0800

    验证码文本字符间距修正

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/CaptchaConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/CaptchaConfig.java
@@ -19,16 +19,27 @@ public class CaptchaConfig
     {
         DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
         Properties properties = new Properties();
+        // 是否有边框  默认为true  我们可以自己设置yes，no
         properties.setProperty("kaptcha.border", "yes");
+        // 边框颜色   默认为Color.BLACK  
         properties.setProperty("kaptcha.border.color", "105,179,90");
+        // 验证码文本字符颜色  默认为Color.BLACK 
         properties.setProperty("kaptcha.textproducer.font.color", "blue");
+        // 验证码图片宽度  默认为200 
         properties.setProperty("kaptcha.image.width", "160");
+        // 验证码图片高度  默认为50
         properties.setProperty("kaptcha.image.height", "60");
-        properties.setProperty("kaptcha.textproducer.font.size", "28");
+        // 验证码文本字符大小  默认为40
+        properties.setProperty("kaptcha.textproducer.font.size", "30");
+        // KAPTCHA_SESSION_KEY
         properties.setProperty("kaptcha.session.key", "kaptchaCode");
-        properties.setProperty("kaptcha.textproducer.char.spac", "35");
+        // 验证码文本字符间距  默认为2
+        properties.setProperty("kaptcha.textproducer.char.space", "3");
+        // 验证码文本字符长度  默认为5
         properties.setProperty("kaptcha.textproducer.char.length", "5");
+        // 验证码文本字体样式  默认为new Font("Arial", 1, fontSize), new Font("Courier", 1, fontSize) 
         properties.setProperty("kaptcha.textproducer.font.names", "Arial,Courier");
+        // 验证码噪点颜色   默认为Color.BLACK
         properties.setProperty("kaptcha.noise.color", "white");
         Config config = new Config(properties);
         defaultKaptcha.setConfig(config);
