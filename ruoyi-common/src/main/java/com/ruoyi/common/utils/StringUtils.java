d30dc35e59d8d85df218c6724d18ba32ca307c2d
RuoYi <yzz_ivy@163.com>
2018-12-29 15:18:28 +0800

    代码格式化

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/StringUtils.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/StringUtils.java
@@ -259,34 +259,44 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils
     /**
      * 下划线转驼峰命名
      */
-    public static String toUnderScoreCase(String str) {
-        if (str == null) {
+    public static String toUnderScoreCase(String str)
+    {
+        if (str == null)
+        {
             return null;
         }
         StringBuilder sb = new StringBuilder();
-        //前置字符是否大写
+        // 前置字符是否大写
         boolean preCharIsUpperCase = true;
-        //当前字符是否大写
+        // 当前字符是否大写
         boolean curreCharIsUpperCase = true;
-        //下一字符是否大写
+        // 下一字符是否大写
         boolean nexteCharIsUpperCase = true;
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++)
+        {
             char c = str.charAt(i);
-            if (i > 0) {
-                preCharIsUpperCase = Character.isUpperCase(str.charAt(i-1));;
-            } else {
+            if (i > 0)
+            {
+                preCharIsUpperCase = Character.isUpperCase(str.charAt(i - 1));
+            }
+            else
+            {
                 preCharIsUpperCase = false;
             }
-            
+
             curreCharIsUpperCase = Character.isUpperCase(c);
 
-            if (i < (str.length() - 1)) {
+            if (i < (str.length() - 1))
+            {
                 nexteCharIsUpperCase = Character.isUpperCase(str.charAt(i + 1));
             }
-            
-            if (preCharIsUpperCase && curreCharIsUpperCase && !nexteCharIsUpperCase) {
+
+            if (preCharIsUpperCase && curreCharIsUpperCase && !nexteCharIsUpperCase)
+            {
                 sb.append(SEPARATOR);
-            }  else if ((i !=0 && !preCharIsUpperCase) && curreCharIsUpperCase) {
+            }
+            else if ((i != 0 && !preCharIsUpperCase) && curreCharIsUpperCase)
+            {
                 sb.append(SEPARATOR);
             }
             sb.append(Character.toLowerCase(c));
