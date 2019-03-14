9bf809061e7d64d593b6bcfc6ff7a3001619da60
RuoYi <yzz_ivy@163.com>
2019-03-14 19:41:26 +0800

    权限校验失败 如果请求为ajax返回json，普通请求跳转页面

--- ruoyi-framework/src/main/java/com/ruoyi/framework/web/exception/GlobalExceptionHandler.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/web/exception/GlobalExceptionHandler.java
@@ -1,14 +1,17 @@
 package com.ruoyi.framework.web.exception;
 
+import javax.servlet.http.HttpServletRequest;
 import org.apache.shiro.authz.AuthorizationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.exception.BusinessException;
 import com.ruoyi.common.exception.DemoModeException;
+import com.ruoyi.common.utils.ServletUtils;
 import com.ruoyi.framework.util.PermissionUtils;
 
 /**
@@ -22,13 +25,22 @@ public class GlobalExceptionHandler
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     /**
-     * 权限校验失败
+     * 权限校验失败 如果请求为ajax返回json，普通请求跳转页面
      */
     @ExceptionHandler(AuthorizationException.class)
-    public AjaxResult handleAuthorizationException(AuthorizationException e)
+    public Object handleAuthorizationException(HttpServletRequest request, AuthorizationException e)
     {
         log.error(e.getMessage(), e);
-        return AjaxResult.error(PermissionUtils.getMsg(e.getMessage()));
+        if (ServletUtils.isAjaxRequest(request))
+        {
+            return AjaxResult.error(PermissionUtils.getMsg(e.getMessage()));
+        }
+        else
+        {
+            ModelAndView modelAndView = new ModelAndView();
+            modelAndView.setViewName("/error/unauth");
+            return modelAndView;
+        }
     }
 
     /**
