90de1067d8300f1fe4623eed32abd51795c9b9dd
RuoYi <yzz_ivy@163.com>
2019-06-12 10:51:05 +0800

    角色导出数据范围表达式添加部门

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/SysRole.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/SysRole.java
@@ -30,8 +30,8 @@ public class SysRole extends BaseEntity
     @Excel(name = "角色排序")
     private String roleSort;
 
-    /** 数据范围（1：所有数据权限；2：自定数据权限） */
-    @Excel(name = "数据范围", readConverterExp = "1=所有数据权限,2=自定义数据权限")
+    /** 数据范围（1：所有数据权限；2：自定义数据权限；3：本部门数据权限；4：本部门及以下数据权限） */
+    @Excel(name = "数据范围", readConverterExp = "1=所有数据权限,2=自定义数据权限,3=本部门数据权限,4=本部门及以下数据权限")
     private String dataScope;
 
     /** 角色状态（0正常 1停用） */
--- sql/ry_20190601.sql
+++ sql/ry_20190601.sql
@@ -106,7 +106,7 @@ create table sys_role (
   role_name 		varchar(30) 	not null 				   comment '角色名称',
   role_key 		    varchar(100) 	not null 				   comment '角色权限字符串',
   role_sort         int(4)          not null                   comment '显示顺序',
-  data_scope        char(1) 	    default '1'				   comment '数据范围（1：全部数据权限 2：自定数据权限）',
+  data_scope        char(1) 	    default '1'				   comment '数据范围（1：全部数据权限 2：自定数据权限 3：本部门数据权限 4：本部门及以下数据权限）',
   status 			char(1) 		not null 			       comment '角色状态（0正常 1停用）',
   del_flag			char(1) 		default '0' 			   comment '删除标志（0代表存在 2代表删除）',
   create_by         varchar(64)     default ''                 comment '创建者',
