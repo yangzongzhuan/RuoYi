8caf674f94a9f1c3a42d6c5fcd074cb7ee79c6ef
RuoYi <yzz_ivy@163.com>
2019-07-08 11:14:20 +0800

    添加summernote富文本字号fontsize

--- ruoyi-admin/src/main/resources/static/ajax/libs/summernote/summernote.js
+++ ruoyi-admin/src/main/resources/static/ajax/libs/summernote/summernote.js
@@ -7547,6 +7547,7 @@
           toolbar: [
               ['style', ['style']],
               ['font', ['bold', 'underline', 'clear']],
+              ['fontsize',['fontsize']],
               ['fontname', ['fontname']],
               ['color', ['color']],
               ['para', ['ul', 'ol', 'paragraph']],
