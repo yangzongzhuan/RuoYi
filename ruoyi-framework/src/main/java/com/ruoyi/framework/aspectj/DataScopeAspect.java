dea394a93de20987bfef6e9c8c51eaffaca36f21
RACSU <racsu@foxmail.com>
2019-07-29 22:23:48 +0800

    解决数据权限只有本部门及以下时候，可能出现数据权限越界的情况

--- ruoyi-framework/src/main/java/com/ruoyi/framework/aspectj/DataScopeAspect.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/aspectj/DataScopeAspect.java
@@ -118,10 +118,9 @@ public class DataScopeAspect
             }
             else if (DATA_SCOPE_DEPT_AND_CHILD.equals(dataScope))
             {
-                String deptChild = user.getDept().getParentId() + "," + user.getDeptId();
                 sqlString.append(StringUtils.format(
-                        " OR {}.dept_id IN ( SELECT dept_id FROM sys_dept WHERE dept_id = {} or ancestors LIKE '%{}%' )",
-                        deptAlias, user.getDeptId(), deptChild));
+                        " OR {}.dept_id IN ( SELECT dept_id FROM sys_dept WHERE dept_id = {} or find_in_set( {} , ancestors ) )",
+                        deptAlias, user.getDeptId(),  user.getDeptId()));
             }
             else if (DATA_SCOPE_SELF.equals(dataScope))
             {
