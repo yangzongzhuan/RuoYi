28b8f3e36fc8043374c8ab6216ed93d1ed9e13f8
RuoYi <yzz_ivy@163.com>
2019-11-21 16:33:34 +0800

    Excel注解dateFormat属性为字符串时间时格式处理

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/poi/ExcelUtil.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/poi/ExcelUtil.java
@@ -225,11 +225,11 @@ public class ExcelUtil<T>
                         else
                         {
                             String dateFormat = field.getAnnotation(Excel.class).dateFormat();
-                            if(StringUtils.isNotEmpty(dateFormat))
+                            if (StringUtils.isNotEmpty(dateFormat))
                             {
-                                val=DateUtils.parseDateToStr(dateFormat,(Date) val);
+                                val = DateUtils.parseDateToStr(dateFormat, (Date) val);
                             }
-                            else 
+                            else
                             {
                                 val = Convert.toStr(val);
                             }
