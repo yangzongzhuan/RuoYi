b5678994dad516cc3525d65947314af0f046af3f
RuoYi <yzz_ivy@163.com>
2020-02-10 14:59:31 +0800

    修改代码生成部分细节问题

--- ruoyi-generator/src/main/java/com/ruoyi/generator/domain/GenTable.java
+++ ruoyi-generator/src/main/java/com/ruoyi/generator/domain/GenTable.java
@@ -3,6 +3,7 @@ package com.ruoyi.generator.domain;
 import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import org.apache.commons.lang3.ArrayUtils;
 import com.ruoyi.common.constant.GenConstants;
 import com.ruoyi.common.core.domain.BaseEntity;
 import com.ruoyi.common.utils.StringUtils;
@@ -262,7 +263,8 @@ public class GenTable extends BaseEntity
     {
         if (isTree(tplCategory))
         {
-            StringUtils.equalsAnyIgnoreCase(javaField, GenConstants.TREE_ENTITY);
+            return StringUtils.equalsAnyIgnoreCase(javaField,
+                    ArrayUtils.addAll(GenConstants.TREE_ENTITY, GenConstants.BASE_ENTITY));
         }
         return StringUtils.equalsAnyIgnoreCase(javaField, GenConstants.BASE_ENTITY);
     }
