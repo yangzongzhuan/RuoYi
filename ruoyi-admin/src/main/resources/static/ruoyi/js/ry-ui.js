18e0a2a27db58692920c8633a3bb24d87d40f3d0
RuoYi <yzz_ivy@163.com>
2020-02-17 15:50:11 +0800

    重置刷新表格树

--- ruoyi-admin/src/main/resources/static/ruoyi/js/ry-ui.js
+++ ruoyi-admin/src/main/resources/static/ruoyi/js/ry-ui.js
@@ -585,6 +585,12 @@ var table = {
                 	} else{
                 	    $("#" + tableId).bootstrapTable('refresh');
                 	}
+            	} else if (table.options.type == table_type.bootstrapTreeTable) {
+            		if($.common.isEmpty(tableId)){
+            	    	$("#" + table.options.id).bootstrapTreeTable('refresh', []);
+                	} else{
+                	    $("#" + tableId).bootstrapTreeTable('refresh', []);
+                	}
             	}
             },
             // 获取选中复选框项
