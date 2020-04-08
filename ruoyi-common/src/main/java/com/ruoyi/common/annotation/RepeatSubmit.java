fe296cc8ba670dbcaa8f106a037ef4bbcd7d6663
RuoYi <yzz_ivy@163.com>
2020-04-08 11:14:16 +0800

    删除无用注解

--- ruoyi-common/src/main/java/com/ruoyi/common/annotation/RepeatSubmit.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/annotation/RepeatSubmit.java
@@ -2,7 +2,6 @@ package com.ruoyi.common.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
