a8eeb652b2e7ec46e2c7401fd5f2761176255c1f
RuoYi <yzz_ivy@163.com>
2020-05-14 14:36:22 +0800

    通用http发送方法增加参数 contentType 编码类型

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/AddressUtils.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/AddressUtils.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.alibaba.fastjson.JSONObject;
 import com.ruoyi.common.config.Global;
+import com.ruoyi.common.constant.Constants;
 import com.ruoyi.common.utils.http.HttpUtils;
 
 /**
@@ -33,7 +34,7 @@ public class AddressUtils
         {
             try
             {
-                String rspStr = HttpUtils.sendGet(IP_URL, "ip=" + ip + "&json=true");
+                String rspStr = HttpUtils.sendGet(IP_URL, "ip=" + ip + "&json=true", Constants.GBK);
                 if (StringUtils.isEmpty(rspStr))
                 {
                     log.error("获取地理位置异常 {}", ip);
@@ -46,7 +47,7 @@ public class AddressUtils
             }
             catch (Exception e)
             {
-                log.error("获取地理位置异常 {}", ip);
+                log.error("获取地理位置异常 {}", e);
             }
         }
         return address;
