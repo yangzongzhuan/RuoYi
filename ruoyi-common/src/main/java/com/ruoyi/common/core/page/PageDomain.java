7728ad9eb4c19ef748d3419c2de821e592304672
RuoYi <yzz_ivy@163.com>
2020-07-04 11:36:59 +0800

    检查字符支持小数点&降级改成异常提醒

--- ruoyi-common/src/main/java/com/ruoyi/common/core/page/PageDomain.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/page/PageDomain.java
@@ -11,10 +11,13 @@ public class PageDomain
 {
     /** 当前记录起始索引 */
     private Integer pageNum;
+
     /** 每页显示记录数 */
     private Integer pageSize;
+
     /** 排序列 */
     private String orderByColumn;
+
     /** 排序的方向 "desc" 或者 "asc". */
     private String isAsc;
 
