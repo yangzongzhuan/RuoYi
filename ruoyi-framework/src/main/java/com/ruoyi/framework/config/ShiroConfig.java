781fd7dd1a806c9f5cd1c7643baf9a0fa1bf38de
Hacker <721806280@qq.com>
2020-07-06 17:48:40 +0800

    update ruoyi-framework/src/main/java/com/ruoyi/framework/config/ShiroConfig.java. java doc

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/ShiroConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/ShiroConfig.java
@@ -45,55 +45,81 @@ public class ShiroConfig
 {
     public static final String PREMISSION_STRING = "perms[\"{0}\"]";
 
-    // Session超时时间，单位为毫秒（默认30分钟）
+    /**
+     * Session超时时间，单位为毫秒（默认30分钟）
+     */
     @Value("${shiro.session.expireTime}")
     private int expireTime;
 
-    // 相隔多久检查一次session的有效性，单位毫秒，默认就是10分钟
+    /**
+     * 相隔多久检查一次session的有效性，单位毫秒，默认就是10分钟
+     */
     @Value("${shiro.session.validationInterval}")
     private int validationInterval;
 
-    // 同一个用户最大会话数
+    /**
+     * 同一个用户最大会话数
+     */
     @Value("${shiro.session.maxSession}")
     private int maxSession;
 
-    // 踢出之前登录的/之后登录的用户，默认踢出之前登录的用户
+    /**
+     * 踢出之前登录的/之后登录的用户，默认踢出之前登录的用户
+     */
     @Value("${shiro.session.kickoutAfter}")
     private boolean kickoutAfter;
 
-    // 验证码开关
+    /**
+     * 验证码开关
+     */
     @Value("${shiro.user.captchaEnabled}")
     private boolean captchaEnabled;
 
-    // 验证码类型
+    /**
+     * 验证码类型
+     */
     @Value("${shiro.user.captchaType}")
     private String captchaType;
 
-    // 设置Cookie的域名
+    /**
+     * 设置Cookie的域名
+     */
     @Value("${shiro.cookie.domain}")
     private String domain;
 
-    // 设置cookie的有效访问路径
+    /**
+     * 设置cookie的有效访问路径
+     */
     @Value("${shiro.cookie.path}")
     private String path;
 
-    // 设置HttpOnly属性
+    /**
+     * 设置HttpOnly属性
+     */
     @Value("${shiro.cookie.httpOnly}")
     private boolean httpOnly;
 
-    // 设置Cookie的过期时间，秒为单位
+    /**
+     * 设置Cookie的过期时间，秒为单位
+     */
     @Value("${shiro.cookie.maxAge}")
     private int maxAge;
 
-    // 设置cipherKey密钥
+    /**
+     * 设置cipherKey密钥
+     */
     @Value("${shiro.cookie.cipherKey}")
     private String cipherKey;
 
-    // 登录地址
+    /**
+     * 登录地址
+     */
     @Value("${shiro.user.loginUrl}")
     private String loginUrl;
 
-    // 权限认证失败地址
+    /**
+     * 权限认证失败地址
+     */
     @Value("${shiro.user.unauthorizedUrl}")
     private String unauthorizedUrl;
 
