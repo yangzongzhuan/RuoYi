f42b8c7a838eea11b0756ac91b4006a89e5d43d8
RuoYi <yzz_ivy@163.com>
2020-07-16 17:09:15 +0800

    定时任务cron表达式验证

--- ruoyi-quartz/src/main/java/com/ruoyi/quartz/controller/SysJobController.java
+++ ruoyi-quartz/src/main/java/com/ruoyi/quartz/controller/SysJobController.java
@@ -21,6 +21,7 @@ import com.ruoyi.common.exception.job.TaskException;
 import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.quartz.domain.SysJob;
 import com.ruoyi.quartz.service.ISysJobService;
+import com.ruoyi.quartz.util.CronUtils;
 
 /**
  * 调度任务信息操作处理
@@ -128,6 +129,10 @@ public class SysJobController extends BaseController
     @ResponseBody
     public AjaxResult addSave(@Validated SysJob job) throws SchedulerException, TaskException
     {
+        if (!CronUtils.isValid(job.getCronExpression()))
+        {
+            return AjaxResult.error("cron表达式不正确");
+        }
         return toAjax(jobService.insertJob(job));
     }
 
@@ -150,6 +155,10 @@ public class SysJobController extends BaseController
     @ResponseBody
     public AjaxResult editSave(@Validated SysJob job) throws SchedulerException, TaskException
     {
+        if (!CronUtils.isValid(job.getCronExpression()))
+        {
+            return AjaxResult.error("cron表达式不正确");
+        }
         return toAjax(jobService.updateJob(job));
     }
 
