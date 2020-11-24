a8b569a24df3a2bfaa30b8b22b6135c4e2309ffe
fatfish517 <fatfish517@163.com>
2020-11-24 10:48:34 +0800

    update ruoyi-common/src/main/java/com/ruoyi/common/utils/reflect/ReflectUtils.java.

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/reflect/ReflectUtils.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/reflect/ReflectUtils.java
@@ -204,6 +204,8 @@ public class ReflectUtils
                             args[i] = DateUtil.getJavaDate((Double) args[i]);
                         }
                     }
+                     else if (cs[i] == boolean.class|| cs[i] == Boolean.class) {
+                        args[i] = Convert.toBool(args[i]);}
                 }
             }
             return (E) method.invoke(obj, args);
