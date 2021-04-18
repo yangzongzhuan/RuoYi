74f35206930d8f1109eec62f1ead7fbfcbed70dd
RuoYi <yzz_ivy@163.com>
2021-04-18 12:30:14 +0800

    修正模板字符编码

--- ruoyi-generator/src/main/java/com/ruoyi/generator/util/VelocityInitializer.java
+++ ruoyi-generator/src/main/java/com/ruoyi/generator/util/VelocityInitializer.java
@@ -22,7 +22,7 @@ public class VelocityInitializer
             // 加载classpath目录下的vm文件
             p.setProperty("file.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
             // 定义字符集
-            p.setProperty(Velocity.ENCODING_DEFAULT, Constants.UTF8);
+            p.setProperty(Velocity.INPUT_ENCODING, Constants.UTF8);
             p.setProperty(Velocity.OUTPUT_ENCODING, Constants.UTF8);
             // 初始化Velocity引擎，指定配置Properties
             Velocity.init(p);
