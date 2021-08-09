fba8008c814fa7e33daac7372ed8a79f06a9f4aa
RuoYi <yzz_ivy@163.com>
2021-08-09 17:36:40 +0800

    移动端进入首页设置默认样式

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysIndexController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysIndexController.java
@@ -60,6 +60,7 @@ public class SysIndexController extends BaseController
         mmap.put("demoEnabled", RuoYiConfig.isDemoEnabled());
         mmap.put("isDefaultModifyPwd", initPasswordIsModify(user.getPwdUpdateDate()));
         mmap.put("isPasswordExpired", passwordIsExpiration(user.getPwdUpdateDate()));
+        mmap.put("isMobile", ServletUtils.checkAgentIsMobile(ServletUtils.getRequest().getHeader("User-Agent")));
 
         // 菜单导航显示风格
         String menuStyle = configService.selectConfigByKey("sys.index.menuStyle");
