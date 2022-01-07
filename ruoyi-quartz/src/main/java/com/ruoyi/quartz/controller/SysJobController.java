871c0a115ddfd678ddad06989cfc31c49dda014a
RuoYi <yzz_ivy@163.com>
2022-01-07 11:26:24 +0800

    修正文字错误

--- ruoyi-quartz/src/main/java/com/ruoyi/quartz/controller/SysJobController.java
+++ ruoyi-quartz/src/main/java/com/ruoyi/quartz/controller/SysJobController.java
@@ -203,7 +203,7 @@ public class SysJobController extends BaseController
         }
         else if (!ScheduleUtils.whiteList(job.getInvokeTarget()))
         {
-            return error("新增任务'" + job.getJobName() + "'失败，目标字符串不在白名单内");
+            return error("修改任务'" + job.getJobName() + "'失败，目标字符串不在白名单内");
         }
         return toAjax(jobService.updateJob(job));
     }
