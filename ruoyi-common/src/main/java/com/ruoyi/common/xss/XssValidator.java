210d75be8e8678e4351b1494ac8779bd655afc65
Awen <39176130+yu1183688986@users.noreply.github.com>
2022-01-30 21:45:20 +0800

    修复了@xss注解字段值为null时的空指针异常问题

--- ruoyi-common/src/main/java/com/ruoyi/common/xss/XssValidator.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/xss/XssValidator.java
@@ -12,15 +12,18 @@ import java.util.regex.Pattern;
  */
 public class XssValidator implements ConstraintValidator<Xss, String>
 {
-    private final String HTML_PATTERN = "<(\\S*?)[^>]*>.*?|<.*? />";
+    private static final String HTML_PATTERN = "<(\\S*?)[^>]*>.*?|<.*? />";
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext)
     {
+        if (StringUtils.isBlank(value)){
+            return true;
+        }
         return !containsHtml(value);
     }
 
-    public boolean containsHtml(String value)
+    public static boolean containsHtml(String value)
     {
         Pattern pattern = Pattern.compile(HTML_PATTERN);
         Matcher matcher = pattern.matcher(value);
