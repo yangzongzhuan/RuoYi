7610e138dc6b2a99c3d9090b50cfc2625a2caead
RuoYi <yzz_ivy@163.com>
2022-04-16 21:41:24 +0800

    字典类型必须以字母开头，且只能为（小写字母，数字，下滑线）

--- ruoyi-common/src/main/java/com/ruoyi/common/core/domain/entity/SysDictType.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/domain/entity/SysDictType.java
@@ -56,6 +56,7 @@ public class SysDictType extends BaseEntity
 
     @NotBlank(message = "字典类型不能为空")
     @Size(min = 0, max = 100, message = "字典类型类型长度不能超过100个字符")
+    @Pattern(regexp = "^[a-z][a-z0-9_]*$", message = "字典类型必须以字母开头，且只能为（小写字母，数字，下滑线）")
     public String getDictType()
     {
         return dictType;
