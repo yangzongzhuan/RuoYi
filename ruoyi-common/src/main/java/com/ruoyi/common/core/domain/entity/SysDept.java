0209ed03262d8de8b70777a62a402d02efd92dc3
RuoYi <yzz_ivy@163.com>
2022-04-25 10:23:17 +0800

    修改显示顺序orderNum类型为整型

--- ruoyi-common/src/main/java/com/ruoyi/common/core/domain/entity/SysDept.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/domain/entity/SysDept.java
@@ -2,6 +2,7 @@ package com.ruoyi.common.core.domain.entity;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
@@ -30,7 +31,7 @@ public class SysDept extends BaseEntity
     private String deptName;
 
     /** 显示顺序 */
-    private String orderNum;
+    private Integer orderNum;
 
     /** 负责人 */
     private String leader;
@@ -95,13 +96,13 @@ public class SysDept extends BaseEntity
         this.deptName = deptName;
     }
 
-    @NotBlank(message = "显示顺序不能为空")
-    public String getOrderNum()
+    @NotNull(message = "显示顺序不能为空")
+    public Integer getOrderNum()
     {
         return orderNum;
     }
 
-    public void setOrderNum(String orderNum)
+    public void setOrderNum(Integer orderNum)
     {
         this.orderNum = orderNum;
     }
