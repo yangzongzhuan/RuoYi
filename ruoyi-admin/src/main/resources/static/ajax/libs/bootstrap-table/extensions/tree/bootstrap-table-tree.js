c2deb1428437f95391168e747a1e4339a351ba80
RuoYi <yzz_ivy@163.com>
2023-08-12 16:22:29 +0800

    树表查询无数据时清除分页信息(I7OB0S)

--- ruoyi-admin/src/main/resources/static/ajax/libs/bootstrap-table/extensions/tree/bootstrap-table-tree.js
+++ ruoyi-admin/src/main/resources/static/ajax/libs/bootstrap-table/extensions/tree/bootstrap-table-tree.js
@@ -217,6 +217,8 @@
             if (!data || data.length <= 0) {
                 var _empty = '<tr><td colspan="' + options.columns.length + '"><div style="display: block;text-align: center;">没有找到匹配的记录</div></td></tr>'
                 $tbody.html(_empty);
+                options.pageNumber = 1;
+                initPagination(0, 0);
                 return;
             }
             // 缓存并格式化数据
@@ -271,6 +273,9 @@
             var pageTo = options.pageNumber * options.pageSize;
             if (pageTo > target.totalRows) {
                 pageTo = target.totalRows;
+            }
+            if (pageFrom > pageTo) {
+                pageFrom = pageTo;
             }
             html.push('<div class="pull-left pagination-detail">');
             html.push('<span class="pagination-info">' + formatShowingRows(pageFrom, pageTo, target.totalRows) + '</span>');
