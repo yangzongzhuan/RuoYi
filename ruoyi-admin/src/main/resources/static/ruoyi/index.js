24d21eda23aa321ea43ec4caf305b64cb8529b2d
RuoYi <yzz_ivy@163.com>
2023-09-14 10:31:10 +0800

    修复横向菜单关闭最后一个页签状态问题(I7SVPK)

--- ruoyi-admin/src/main/resources/static/ruoyi/index.js
+++ ruoyi-admin/src/main/resources/static/ruoyi/index.js
@@ -73,9 +73,10 @@ $(window).bind("load resize", function() {
 });
 
 function syncMenuTab(dataId) {
-	if(isLinkage) {
+    if(isLinkage) {
         var $dataObj = $('a[href$="' + decodeURI(dataId) + '"]');
         if ($dataObj.attr("class") != null && !$dataObj.hasClass("noactive")) {
+            $('.tab-pane li').removeClass("active");
             $('.nav ul').removeClass("in");
             $dataObj.parents("ul").addClass("in")
             $dataObj.parents("li").addClass("active").siblings().removeClass("active").find('li').removeClass("active");
@@ -340,6 +341,7 @@ $(function() {
         var panelUrl = $(this).parents('.menuTab').data('panel');
         // 当前元素处于活动状态
         if ($(this).parents('.menuTab').hasClass('active')) {
+        	syncMenuTab($('.page-tabs-content').find('.active').attr('data-id'));
 
             // 当前元素后面有同辈元素，使后面的一个元素处于活动状态
             if ($(this).parents('.menuTab').next('.menuTab').length) {
