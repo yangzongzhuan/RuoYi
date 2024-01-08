f84b77993c11b9c3b7040c3f67a2dd429f19e740
coderwu <coderwumail@gmail.com>
2024-01-08 22:24:56 +0800

    更新HttpUtils中的User-Agent

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/http/HttpUtils.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/http/HttpUtils.java
@@ -74,7 +74,7 @@ public class HttpUtils
             URLConnection connection = realUrl.openConnection();
             connection.setRequestProperty("accept", "*/*");
             connection.setRequestProperty("connection", "Keep-Alive");
-            connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            connection.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
             connection.connect();
             in = new BufferedReader(new InputStreamReader(connection.getInputStream(), contentType));
             String line;
