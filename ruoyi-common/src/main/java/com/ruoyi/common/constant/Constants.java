fd37e8b4a71f6ac100c50e9c7494e8229ff9c53c
RuoYi <yzz_ivy@163.com>
2024-03-11 11:15:11 +0800

    定时任务白名单配置范围缩小

--- ruoyi-common/src/main/java/com/ruoyi/common/constant/Constants.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/constant/Constants.java
@@ -105,7 +105,7 @@ public class Constants
     /**
      * 定时任务白名单配置（仅允许访问的包名，如其他需要可以自行添加）
      */
-    public static final String[] JOB_WHITELIST_STR = { "com.ruoyi" };
+    public static final String[] JOB_WHITELIST_STR = { "com.ruoyi.quartz.task" };
 
     /**
      * 定时任务违规的字符
