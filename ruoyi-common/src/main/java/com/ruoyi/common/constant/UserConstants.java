05e2c2834332171a1df1ed6031a521c5fdcc0525
RuoYi <yzz_ivy@163.com>
2024-09-21 11:03:13 +0800

    修复角色禁用权限不失效问题

--- ruoyi-common/src/main/java/com/ruoyi/common/constant/UserConstants.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/constant/UserConstants.java
@@ -21,6 +21,9 @@ public class UserConstants
     /** 用户封禁状态 */
     public static final String USER_DISABLE = "1";
 
+    /** 角色正常状态 */
+    public static final String ROLE_NORMAL = "0";
+
     /** 角色封禁状态 */
     public static final String ROLE_DISABLE = "1";
 
