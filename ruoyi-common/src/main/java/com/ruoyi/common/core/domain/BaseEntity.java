e728c37b6424ade9bd7e28898bde6ff531015fe4
RuoYi <yzz_ivy@163.com>
2024-11-12 13:07:43 +0800

    add timezone

--- ruoyi-common/src/main/java/com/ruoyi/common/core/domain/BaseEntity.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/domain/BaseEntity.java
@@ -25,14 +25,14 @@ public class BaseEntity implements Serializable
     private String createBy;
 
     /** 创建时间 */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
 
     /** 更新者 */
     private String updateBy;
 
     /** 更新时间 */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 
     /** 备注 */
