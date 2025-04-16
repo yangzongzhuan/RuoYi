bbeb6d3b28c0b9fc85093ce8fbb4b3d7b0b8d19b
RuoYi <yzz_ivy@163.com>
2025-04-16 12:05:20 +0800

    进入授权角色页校验数据权限

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysUserController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysUserController.java
@@ -247,6 +247,7 @@ public class SysUserController extends BaseController
     @GetMapping("/authRole/{userId}")
     public String authRole(@PathVariable("userId") Long userId, ModelMap mmap)
     {
+        userService.checkUserDataScope(userId);
         SysUser user = userService.selectUserById(userId);
         // 获取用户所属的角色列表
         List<SysRole> roles = roleService.selectRolesByUserId(userId);
