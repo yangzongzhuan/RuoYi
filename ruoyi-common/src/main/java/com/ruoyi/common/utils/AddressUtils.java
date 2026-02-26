51f6fcbc869d8e69db485dfdd1f33c11425494ba
RuoYi <yzz_ivy@163.com>
2026-02-26 10:09:16 +0800

    update address url

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/AddressUtils.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/AddressUtils.java
@@ -17,7 +17,7 @@ public class AddressUtils
     private static final Logger log = LoggerFactory.getLogger(AddressUtils.class);
 
     // IP地址查询
-    public static final String IP_URL = "http://whois.pconline.com.cn/ipJson.jsp";
+    public static final String IP_URL = "https://whois.pconline.com.cn/ipJson.jsp";
 
     // 未知地址
     public static final String UNKNOWN = "XX XX";
