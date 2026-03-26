a4888a3eddd3f38a09ed996805e67865670f5ef5
RuoYi <yzz_ivy@163.com>
2026-03-26 22:48:58 +0800

    update application

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -17,7 +17,7 @@ public class RuoYiApplication
         SpringApplication.run(RuoYiApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
                 " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\124   /  /    \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
                 " | ( ' )  |       \\  _. /  '       \n" +
                 " |(_ o _) /        _( )_ .'         \n" +
                 " | (_,_).' __  ___(_ o _)'          \n" +
