94e558ea4ae5139217a3a9775f7cc3c5f6c99288
RuoYi <yzz_ivy@163.com>
2026-04-03 15:35:37 +0800

    数据权限注解支持自定义字段名

--- ruoyi-common/src/main/java/com/ruoyi/common/annotation/DataScope.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/annotation/DataScope.java
@@ -17,14 +17,24 @@ import java.lang.annotation.Target;
 public @interface DataScope
 {
     /**
+     * 用户表的别名
+     */
+    public String userAlias() default "";
+
+    /**
      * 部门表的别名
      */
     public String deptAlias() default "";
 
     /**
-     * 用户表的别名
+     * 用户字段名
      */
-    public String userAlias() default "";
+    public String userField() default "user_id";
+
+    /**
+     * 部门字段名
+     */
+    public String deptField() default "dept_id";
 
     /**
      * 权限字符（用于多个角色匹配符合要求的权限）默认根据权限注解@RequiresPermissions获取，多个权限用逗号分隔开来
